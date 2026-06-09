c0dd20c88ec19b7bca51b3bd521ff66e6dc10201
masterschetanapp-creator <masterschetanapp@gmail.com>
2026-06-09 11:36:21 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.js
+// Paste your Firebase Web App configuration below.
+// You can find this in the Firebase Console under Project Settings -> Web App.
+// Alternatively, you can configure Firebase directly inside the app's Save/Load DB settings.
+
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyD1UrnzXeqJIxJNzUCqCc9I5ibGgqD2rbg",
+  authDomain: "calltrack-crm-18a5a.firebaseapp.com",
+  databaseURL: "https://calltrack-crm-18a5a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "calltrack-crm-18a5a",
+  storageBucket: "calltrack-crm-18a5a.firebasestorage.app",
+  messagingSenderId: "279088552928",
+  appId: "1:279088552928:web:00a0025031c6fc06df2985"
+};
